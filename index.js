fac414a1ac2d3cebd4b6b5598cfa91e66e0cf40c
Your Name <your_email@example.com>
2024-05-31 17:13:51 +0530

    cluster

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const PORT = 8000;
+const app = express();
+
+app.get("/",(req,res)=>{
+    return res.json({message:`Hello from Express Server ${process.pid}`})
+})
+
+app.listen(PORT,()=>console.log(`Server Started At PORT: ${PORT}`));
